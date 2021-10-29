936c9e08a77e73f53e732c9fb291cb18f6a6b741
cybworkspace <cybworkspace@gmail.com>
2021-10-28 23:12:48 -0700

    nvsh pos_emb

--- models/nvs/nvsh_ttp.py
+++ models/nvs/nvsh_ttp.py
@@ -10,11 +10,13 @@ from .base_nvs_hypernet import BaseNvsHypernet
 @register('nvsh-ttp')
 class NvshTtp(BaseNvsHypernet):
 
-    def __init__(self, input_size, patch_size, dtoken_dim, hyponet_spec, ttp_net_spec):
+    def __init__(self, input_size, patch_size, dtoken_dim, hyponet_spec, ttp_net_spec, use_pos_emb=False):
         super().__init__(hyponet_spec)
         self.patch_size = patch_size
         self.prefc = nn.Linear(patch_size**2 * 9, dtoken_dim)
-        # self.pos_emb = nn.Parameter(torch.randn(1, (input_size // patch_size)**2, dtoken_dim))
+        self.use_pos_emb = use_pos_emb
+        if use_pos_emb:
+            self.pos_emb = nn.Parameter(torch.randn(1, (input_size // patch_size)**2, dtoken_dim))
         self.ttp_net = models.make(ttp_net_spec, args={'dtoken_dim': dtoken_dim, 'hyponet': self.hyponet})
 
     def generate_params(self, rays_o, rays_d, imgs):
@@ -26,7 +28,8 @@ class NvshTtp(BaseNvsHypernet):
         x = F.unfold(x, P, stride=P) # (B * N, P * P * 9, (H // P) * (W // P))
         x = x.permute(0, 2, 1).contiguous().view(B, N * (H // P) * (W // P), -1)
         x = self.prefc(x)
-        # x = x + self.pos_emb.repeat(1, N, 1)
+        if self.use_pos_emb:
+            x = x + self.pos_emb.repeat(1, N, 1)
         params = self.ttp_net(x)
         return params
 
